33ffbeebbd507191bf4d1b5405465a056eff7047
joshdpark <joshua.park@clarifyhealth.com>
2023-02-06 09:30:25 -0500

    Add comments for configs and add exec-path-from-shell and magit

--- init.el
+++ init.el
@@ -4,13 +4,15 @@
 (package-initialize)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(repeat-mode 1)
-(electric-pair-mode 1)
-(require 'ess-r-mode)
-;; LSP
+(repeat-mode 1) ;; allow for repeats certain commands
+(electric-pair-mode 1) ;; auto-add brackets
+(use-package ess-site
+  :ensure t)
+;; LSP configuration and setup -- see performance notes for lsp mode
 (setq read-process-output-max (expt 2 20)) ;; 1mb
 (setq gc-cons-threshold (expt 2 24))
 (use-package lsp-mode
+  :ensure t
   :init
   ;; set prefix for lsp-command-keymap
   (setq lsp-keymap-prefix "C-c l")
@@ -25,8 +27,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(monstera))
  '(custom-safe-themes
-   '("d652cb8c095485bee329ee0a34e4a28de77f7bfdd192b8934685dffa477c16fc" default))
- '(package-selected-packages '(company lsp-haskell lsp-ui lsp-mode flycheck slime ess)))
+   '("b56f30864c92a96e2a6aa9a953707725a5c35f0d9f37061f772c53b7a096c036" "d652cb8c095485bee329ee0a34e4a28de77f7bfdd192b8934685dffa477c16fc" default))
+ '(package-selected-packages
+   '(exec-path-from-shell magit company lsp-haskell lsp-ui lsp-mode flycheck slime ess)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
