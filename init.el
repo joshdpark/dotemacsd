adea7619d00921db9a8d76c0973163c4279adb9b
joshdpark <joshua.park@clarifyhealth.com>
2023-02-07 11:02:06 -0500

    remove elpa just keep init.el

--- init.el
+++ init.el
@@ -4,10 +4,116 @@
 (package-initialize)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
+(scroll-bar-mode -1)
 (repeat-mode 1) ;; allow for repeats certain commands
-(electric-pair-mode 1) ;; auto-add brackets
-(use-package ess-site
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize)) ;; set PATH to shell PATH
+(setq-default indent-tabs-mode nil) ;; spaces for tabs
+
+;; Meow setup
+(defun meow-setup ()
+  (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+  (meow-motion-overwrite-define-key
+   '("j" . meow-next)
+   '("k" . meow-prev)
+   '("<escape>" . ignore))
+  (meow-leader-define-key
+   ;; SPC j/k will run the original command in MOTION state.
+   '("j" . "H-j")
+   '("k" . "H-k")
+   '("u" . "C-u")
+   ;; Use SPC (0-9) for digit arguments.
+   '("1" . meow-digit-argument)
+   '("2" . meow-digit-argument)
+   '("3" . meow-digit-argument)
+   '("4" . meow-digit-argument)
+   '("5" . meow-digit-argument)
+   '("6" . meow-digit-argument)
+   '("7" . meow-digit-argument)
+   '("8" . meow-digit-argument)
+   '("9" . meow-digit-argument)
+   '("0" . meow-digit-argument)
+   '("/" . meow-keypad-describe-key)
+   '("?" . meow-cheatsheet))
+  (meow-normal-define-key
+   '("0" . meow-expand-0)
+   '("9" . meow-expand-9)
+   '("8" . meow-expand-8)
+   '("7" . meow-expand-7)
+   '("6" . meow-expand-6)
+   '("5" . meow-expand-5)
+   '("4" . meow-expand-4)
+   '("3" . meow-expand-3)
+   '("2" . meow-expand-2)
+   '("1" . meow-expand-1)
+   '("-" . negative-argument)
+   '(";" . meow-reverse)
+   '("," . meow-inner-of-thing)
+   '("." . meow-bounds-of-thing)
+   '("[" . meow-beginning-of-thing)
+   '("]" . meow-end-of-thing)
+   '("a" . meow-append)
+   '("A" . meow-open-below)
+   '("b" . meow-back-word)
+   '("B" . meow-back-symbol)
+   '("c" . meow-change)
+   '("d" . meow-delete)
+   '("D" . meow-backward-delete)
+   '("e" . meow-next-word)
+   '("E" . meow-next-symbol)
+   '("f" . meow-find)
+   '("g" . meow-cancel-selection)
+   '("G" . meow-grab)
+   '("h" . meow-left)
+   '("H" . meow-left-expand)
+   '("i" . meow-insert)
+   '("I" . meow-open-above)
+   '("j" . meow-next)
+   '("J" . meow-next-expand)
+   '("k" . meow-prev)
+   '("K" . meow-prev-expand)
+   '("l" . meow-right)
+   '("L" . meow-right-expand)
+   '("m" . meow-join)
+   '("n" . meow-search)
+   '("o" . meow-block)
+   '("O" . meow-to-block)
+   '("p" . meow-yank)
+   '("P" . meow-yank-pop)
+   '("q" . meow-quit)
+   '("Q" . meow-goto-line)
+   '("r" . meow-replace)
+   '("R" . meow-swap-grab)
+   '("s" . meow-kill)
+   '("t" . meow-till)
+   '("u" . meow-undo)
+   '("U" . meow-undo-in-selection)
+   '("v" . meow-visit)
+   '("w" . meow-mark-word)
+   '("W" . meow-mark-symbol)
+   '("x" . meow-line)
+   '("X" . meow-goto-line)
+   '("y" . meow-save)
+   '("Y" . meow-sync-grab)
+   '("z" . meow-pop-selection)
+   '("'" . repeat)
+   '("<escape>" . ignore)))
+
+(require 'meow)
+(meow-setup)
+(meow-global-mode 1)
+
+(use-package ess
   :ensure t)
+(use-package corfu
+  :ensure t
+  :init
+  (global-corfu-mode))
+(use-package vertico
+  :ensure t
+  :init
+  (vertico-mode))
+
 ;; LSP configuration and setup -- see performance notes for lsp mode
 (setq read-process-output-max (expt 2 20)) ;; 1mb
 (setq gc-cons-threshold (expt 2 24))
@@ -29,7 +135,7 @@
  '(custom-safe-themes
    '("b56f30864c92a96e2a6aa9a953707725a5c35f0d9f37061f772c53b7a096c036" "d652cb8c095485bee329ee0a34e4a28de77f7bfdd192b8934685dffa477c16fc" default))
  '(package-selected-packages
-   '(exec-path-from-shell magit company lsp-haskell lsp-ui lsp-mode flycheck slime ess)))
+   '(vertico corfu org json-mode ess-site meow exec-path-from-shell magit lsp-haskell lsp-ui lsp-mode flycheck slime ess)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
