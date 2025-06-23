1ca6d1d80586f8c64ed1d1e3870e6dd6cd20d45d
Joshua Park <josh.d.park@gmail.com>
2025-06-23 09:13:52 -0400

    monstera theme changes

--- custom.el
+++ custom.el
@@ -5,43 +5,18 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(monstera))
  '(custom-safe-themes
-   '("aa83a9d89a0a90120d9f03dccdeb9409738e0801202a90dec673b1a8ab0d1a2a" "4bcc3f12b2bbaee02924318562fd40958283c45c3983231a1b040ea18ef5c601" default))
- '(meow-mode-state-list
-   '((authinfo-mode . normal)
-     (beancount-mode . normal)
-     (bibtex-mode . normal)
-     (cider-repl-mode . normal)
-     (cider-test-report-mode . normal)
-     (cider-browse-spec-view-mode . motion)
-     (cargo-process-mode . normal)
-     (conf-mode . normal)
-     (deadgrep-edit-mode . normal)
-     (deft-mode . normal)
-     (diff-mode . normal)
-     (ediff-mode . motion)
-     (gud-mode . normal)
-     (haskell-interactive-mode . normal)
-     (help-mode . motion)
-     (helpful-mode . motion)
-     (json-mode . normal)
-     (jupyter-repl-mode . normal)
-     (mix-mode . normal)
-     (occur-edit-mode . normal)
-     (pass-view-mode . normal)
-     (prog-mode . normal)
-     (py-shell-mode . normal)
-     (restclient-mode . normal)
-     (telega-chat-mode . normal)
-     (term-mode . normal)
-     (text-mode . normal)
-     (vterm-mode . normal)
-     (Custom-mode . normal)))
+   '("f62bd44001f2677661792ca74d5b99e48ded3383911a818ddb2308547f1943d9" "a7c1300b6372cb2830ad444664cee91b27c5db6e42ddbfa892774409216f9a54" "82e8ffc784490e98a334755803e486cc9f1aada7744190737f62a215e661f1c1" "aa83a9d89a0a90120d9f03dccdeb9409738e0801202a90dec673b1a8ab0d1a2a" "4bcc3f12b2bbaee02924318562fd40958283c45c3983231a1b040ea18ef5c601" default))
+ '(exec-path
+   '("/usr/bin/" "/bin/" "/usr/sbin/" "/sbin/" "/Applications/Emacs.app/Contents/MacOS/libexec/" "/Users/joshuapark/.local/bin"))
+ '(org-agenda-files '("~/sharedconfig/todo.org"))
  '(package-selected-packages
-   '(zig-mode geiser-racket racket-mode vterm exec-path-from-shell magit ess haskell-mode use-package corfu meow pyvenv vertico)))
+   '(consult cape eat pdf-tools sicp geiser-chez geiser-racket pyvenv))
+ '(zig-zig-bin "/Users/joshuapark/.local/share/zig/zig"))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 140 :width normal :foundry "nil" :family "Berkeley Mono"))))
+ '(font-lock-keyword-face ((t (:inherit modus-themes-bold :foreground "#5317ac")))))
